6b57a4e86194ca1e7733e299ac6a44689abde9a7
Dmitry Dorokhov <b27011971@inbox.ru>
2023-11-01 12:38:56 +0300

    Creating a Controller for application requests

---
+++ src/main/java/ru/dorokhov/spring/controller/ProductController.java
@@ -0,0 +1,40 @@
+package ru.dorokhov.spring.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.dorokhov.spring.dto.ProductDTO;
+import ru.dorokhov.spring.entity.Product;
+import ru.dorokhov.spring.service.ProductService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody ProductDTO dto) {
+        return new ResponseEntity<>(productService.create(dto), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> readAll() {
+        return new ResponseEntity<>(productService.readAll(), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Product> update(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.update(product), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{/id}")
+    public HttpStatus delete(@PathVariable Long id) {
+        productService.delete(id);
+        return HttpStatus.OK;
+    }
+
+}
