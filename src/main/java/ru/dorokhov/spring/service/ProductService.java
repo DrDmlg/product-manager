626d8c0df663c3d62e600918efed2da68985815c
Dmitry Dorokhov <b27011971@inbox.ru>
2023-11-01 12:00:52 +0300

    Some fix

--- src/main/java/ru/dorokhov/spring/service/ProductService.java
+++ src/main/java/ru/dorokhov/spring/service/ProductService.java
@@ -1,2 +1,35 @@
-package ru.dorokhov.spring.service;public class ProductService {
+package ru.dorokhov.spring.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.dorokhov.spring.dto.ProductDTO;
+import ru.dorokhov.spring.entity.Product;
+import ru.dorokhov.spring.repository.ProductRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Product create(ProductDTO dto) {
+        return productRepository.save(Product.builder()
+                .name(dto.getName())
+                .amount(dto.getAmount())
+                .build());
+    }
+
+    public List<Product> readAll() {
+        return productRepository.findAll();
+    }
+
+    public Product update(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
 }
